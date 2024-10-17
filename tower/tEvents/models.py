2e6d5b8e01438592fea485ab38ad76198c572622
William Edwards <williamedwards7717@gmail.com>
2024-10-17 11:41:25 -0600

    models

--- tower/tEvents/models.py
+++ tower/tEvents/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class TEvent(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    coverImg = models.CharField(max_length=1000)
+    location = models.CharField(max_length=300)
+    capacity = models.IntegerField()
+    startDate = models.DateTimeField()
+    isCanceled = models.BooleanField(default=False)
+    EventType = models.TextChoices("EventType", "concert convetion sport digital")
+    type = models.CharField(blank=True, choices=EventType, max_length=10)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+
+    def __str__(self):
+        return self.name
+
+class Ticket(models.Model):
+    isPremium = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tEvent = models.ForeignKey(TEvent, on_delete=models.CASCADE)
+
+
+class Comments(models.Model):
+    body = models.TextField(max_length=1000)
+    isAttending = models.BooleanField()
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+    tEvent = models.ForeignKey(TEvent, on_delete=models.CASCADE)
