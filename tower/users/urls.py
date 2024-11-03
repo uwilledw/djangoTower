90ca82b8197fd765fe4a6aeed350488b8c7b7341
William Edwards <williamedwards7717@gmail.com>
2024-11-02 23:45:56 -0600

    login form

--- tower/users/urls.py
+++ tower/users/urls.py
@@ -4,6 +4,8 @@ from . import views
 app_name = 'users'
 
 urlpatterns = [
+    path('register/', views.register_view, name="register"),
     path('login/', views.login_view, name="login"),
-    
+    path('logout/', views.logout_view, name="logout"),
+
 ]
